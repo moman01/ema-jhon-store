53445dcc805ca528fdd27c20e003d85cdc12a343
Minhajul Abedin <learnermoman@gmail.com>
2023-03-29 23:34:31 +0600

    navbar added

--- src/compopnents/Header/Header.jsx
+++ src/compopnents/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import  logo from'../../images/Logo.svg';
 const Header = () => {
   return (
     <div>
       <nav className='header'>
-        <img src="" alt="" />
+        <img src={logo} alt="" />
         <div>
-          <a href=""></a><a href=""></a><a href=""></a><a href=""></a>
+          <a href="/Shop">Shop</a>
+          <a href="/Order">Order</a>
+          <a href="/Inventory">Inventory</a>
+          <a href="/Login">Login</a>
         </div>
       </nav>
     </div>
