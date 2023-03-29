758d104e8039b67d895f46b8123538e5c2c0bb86
Minhajul Abedin <learnermoman@gmail.com>
2023-03-29 22:45:08 +0600

    ema john created

---
+++ src/compopnents/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+  return (
+    <div>
+      <nav className='header'>
+        <img src="" alt="" />
+        <div>
+          <a href=""></a><a href=""></a><a href=""></a><a href=""></a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
